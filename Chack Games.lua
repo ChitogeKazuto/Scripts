23b844a2d97e4aa4cc8dc17ec351c50e4eccc6fe
ChitogeKazuto <70831261+ChitogeKazuto@users.noreply.github.com>
2020-12-21 20:48:33 +0800

    Update

---
+++ Chack Games.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 0 then
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/ChitogeKazuto/Scripts/main/Mochi%20Hub%20-%20Universal.lua'),true))()
+else
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/ChitogeKazuto/Scripts/main/Mochi%20Hub%20-%20Universal.lua'),true))()
+    end
