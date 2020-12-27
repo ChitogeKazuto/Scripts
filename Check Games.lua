a6e7872807bc217e50268dd75bb665d5ff76eb09
ChitogeKazuto <70831261+ChitogeKazuto@users.noreply.github.com>
2020-12-27 12:40:59 +0700

    Update Check Games.lua

--- Check Games.lua
+++ Check Games.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 2976140098 then
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/ChitogeKazuto/Scripts/main/Mochi%20Hub%20-%20Universal.lua'),true))()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/ChitogeKazuto/Scripts/main/Mochi%20Hub%20-%20VoxlBlade.lua'),true))()
 else
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/ChitogeKazuto/Scripts/main/Mochi%20Hub%20-%20Universal.lua'),true))()
     end
